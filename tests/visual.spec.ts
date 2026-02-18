19c0afa48514d9c5ad62bcc566d9e64c66cefa23
Ihor Pisnyi <v-ihor.pisnyi@shutterfly.com>
2026-02-18 17:49:59 +0200

    test

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -19,21 +19,15 @@ const stories = JSON.parse(fileContent)
 
 for (const story of stories)
   test(`visual ${story}`, async ({ page, request }) => {
-    await page.addInitScript(() => {
-      const remove = () => {
-        const el = document.querySelector('[href*="whats-new"]')
-        if (el) el.remove()
-      }
-
-      // Remove immediately
-      remove()
-
-      // Remove if it appears later
-      const observer = new MutationObserver(remove)
-      observer.observe(document.body, { childList: true, subtree: true })
-    })
-
     await page.goto(`http://localhost:6006/?path=/story/${story}`)
+
+    await page.addStyleTag({
+      content: `
+      [href*="whats-new"] {
+        visibility: hidden !important;
+      }
+    `,
+    })
 
     const locator = page
       .frameLocator('#storybook-preview-iframe')
